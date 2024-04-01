680b1ee06991aa1386b01ae60dbac0d1faff8292
Stefan Gostić <stefan.gostic@propelmedia.com>
2024-04-01 11:58:34 +0200

    Refactor getMaterials function and remove API call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import styles from "./page.module.scss";
 import LeftSide from "@/components/LeftSide/LeftSide";
 import RightSide from "@/components/RightSide/RightSide";
+import { Material } from "@/utils/types";
 
-const getMaterials = async () => {
-  const res = await fetch(process.env.NEXT_PUBLIC_URL + "/api/materials");
-  const data = await res.json();
-  return data;
-};
+// const getMaterials = async () => {
+//   const res = await fetch(process.env.NEXT_PUBLIC_URL + "/api/materials");
+//   const data = await res.json();
+//   return data;
+// };
 
 export default async function Home() {
-  const materials = (await getMaterials()) || [];
+  const materials: Material[] = [];
 
   return (
     <main className={styles.main}>
